bcd8c55df22227fdfd930328b4dd26f7ef844827
Bharat Vasnani <31435621+Bharatdev1@users.noreply.github.com>
2025-02-27 20:40:33 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,7 +29,7 @@ const Contact = () => {
               </div>
               <div className="d-flex align-items-center">
                 <Mail className="me-2" />
-                <p className="mb-0">contact@luxeshades.com</p>
+                <p className="mb-0">contactsarthi@proton.me</p>
               </div>
             </div>
           </div>
